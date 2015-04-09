e5592b232bdb0447449af1b7ee68e15991d15e73
Josh Pollock <jpollock412@gmail.com>
2015-04-08 23:52:23 -0400

    add inline docs

--- core.php
+++ core.php
@@ -5,7 +5,7 @@
  * Description: Runs an action on submit
  * Version:     1.0.0
  * Author:      David Cramer
- * Author URI:  
+ * Author URI:
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Translation Domain: cf-run-action
@@ -13,7 +13,15 @@
 
 
 add_filter('caldera_forms_get_form_processors', 'cf_run_action_register_processor');
-
+/**
+ * Add processor
+ *
+ * @uses "caldera_forms_get_form_processors" filter
+ *
+ * @since 1.0.0
+ *
+ * @return array Processors
+ */
 function cf_run_action_register_processor($pr){
 	$pr['run_action'] = array(
 		"name"              =>  __('Run Action'),
@@ -26,13 +34,28 @@ function cf_run_action_register_processor($pr){
 
 	return $pr;
 }
-function cf_run_action_process($config, $form){
+
+/**
+ * Callback function for the processor
+ *
+ * @since 1.0.0
+ *
+ * @param array $config Processor settings. Key 'action' has action name.
+ * @param array $form Form submission data.
+ */
+function cf_run_action_process( $config, $form){
 	
 	$data = array();
 	foreach($form['fields'] as $field_id=>$field){
 		$data[$field['slug']] = Caldera_Forms::get_field_data($field_id, $form);
 	}
 
+	/**
+	 * The action this processor calls. Named according to form setting.
+	 *
+	 * @param array $data Array of form data, keyed by field ID.
+	 * @param array $form Form submission data.
+	 */
 	do_action($config['action'], $data, $form);
 
 }
