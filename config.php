77836d5ea74e158059d3fefae8482ef83c85c5a8
David Cramer <david@digilab.co.za>
2015-04-30 14:17:57 +0200

    add pre and post type process.

--- config.php
+++ config.php
@@ -1,5 +1,29 @@
 <div class="caldera-config-group">
-	<label><?php echo __('Action'); ?> </label>
+	<label><?php echo __('Position'); ?> </label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[position]">
+
+		<option value="pre" {{#is position value="pre"}}selected="selected"{{/is}}><?php _e('Pre-Process'); ?></option>
+		<option value="process" {{#is position value="process"}}selected="selected"{{/is}}><?php _e('Process'); ?></option>
+		<option value="post" {{#is position value="post"}}selected="selected"{{/is}}><?php _e('Post-Process'); ?></option>
+
+		</select>
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label><?php echo __('type'); ?> </label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[type]">
+
+			<option value="filter" {{#is type value="filter"}}selected="selected"{{/is}}><?php _e('Filter'); ?></option>
+			<option value="action" {{#is type value="action"}}selected="selected"{{/is}}><?php _e('Action'); ?></option>
+
+		</select>
+	</div>
+</div>
+
+<div class="caldera-config-group">
+	<label><?php echo __('Action / Filter'); ?> </label>
 	<div class="caldera-config-field">
 		<input type="text" class="block-input field-config" name="{{_name}}[action]" value="{{action}}">
 	</div>
--- core.php
+++ core.php
@@ -28,12 +28,57 @@ function cf_run_action_register_processor($pr){
 		"description"       =>  __("Run Action on submission"),
 		"author"            =>  'David Cramer',
 		"author_url"        =>  'http://cramer.co.za',
+		"pre_processor"     =>  'cf_run_action_pre_process',
 		"processor"         =>  'cf_run_action_process',
+		"post_processor"    =>  'cf_run_action_post_process',
 		"template"          =>  plugin_dir_path(__FILE__) . "config.php",
 	);
 
 	return $pr;
 }
+
+/**
+ * Callback function for the pre processor
+ *
+ * @since 1.0.0
+ *
+ * @param array $config Processor settings. Key 'action' has action name.
+ * @param array $form Form submission data.
+ */
+function cf_run_action_pre_process( $config, $form){
+	
+	// comatability with old version and standard check.
+	if( !isset( $config['position'] ) || $config['position'] !== 'pre' ){
+		return;
+	}
+
+	$data = array();
+	foreach($form['fields'] as $field_id=>$field){
+		$data[$field['slug']] = Caldera_Forms::get_field_data($field_id, $form);
+	}
+
+	if( $config['type'] == 'filter' ){
+		/**
+		 * The filter this pre processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		if( has_filter( $config['action'] ) ){
+			return apply_filters($config['action'], $data, $form);
+		}
+	}else{
+		/**
+		 * The action this pre processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		do_action($config['action'], $data, $form);
+	}
+
+}
+
 
 /**
  * Callback function for the processor
@@ -44,19 +89,81 @@ function cf_run_action_register_processor($pr){
  * @param array $form Form submission data.
  */
 function cf_run_action_process( $config, $form){
+
+	// comatability with old version and standard check.
+	if( isset( $config['position'] ) && $config['position'] !== 'process' ){
+		return;
+	}
 	
 	$data = array();
 	foreach($form['fields'] as $field_id=>$field){
 		$data[$field['slug']] = Caldera_Forms::get_field_data($field_id, $form);
 	}
 
-	/**
-	 * The action this processor calls. Named according to form setting.
-	 *
-	 * @param array $data Array of form data, keyed by field ID.
-	 * @param array $form Form submission data.
-	 */
-	do_action($config['action'], $data, $form);
+	if( isset( $config['type'] ) && $config['type'] == 'filter' && has_filter( $config['action'] ) ){
+		/**
+		 * The filter this processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		if( has_filter( $config['action'] ) ){
+			return apply_filters($config['action'], $data, $form);
+		}
+
+	}else{
+		/**
+		 * The action this processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		do_action($config['action'], $data, $form);
+	}
+
+}
+
+
+/**
+ * Callback function for the post processor
+ *
+ * @since 1.0.0
+ *
+ * @param array $config Processor settings. Key 'action' has action name.
+ * @param array $form Form submission data.
+ */
+function cf_run_action_post_process( $config, $form){
+	
+	// comatability with old version and standard check.
+	if( !isset( $config['position'] ) || $config['position'] !== 'post' ){
+		return;
+	}	
+	
+	$data = array();
+	foreach($form['fields'] as $field_id=>$field){
+		$data[$field['slug']] = Caldera_Forms::get_field_data($field_id, $form);
+	}
+
+	if( $config['type'] == 'filter' ){
+		/**
+		 * The filter this post processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		if( has_filter( $config['action'] ) ){
+			return apply_filters($config['action'], $data, $form);
+		}
+
+	}else{
+		/**
+		 * The action this post processor calls. Named according to form setting.
+		 *
+		 * @param array $data Array of form data, keyed by field ID.
+		 * @param array $form Form submission data.
+		 */
+		do_action($config['action'], $data, $form);
+	}
 
 }
 
